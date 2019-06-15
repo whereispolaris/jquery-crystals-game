d65906df77a21a013c8a39058c0e83182ce8df4d
Santiago Enciso <whereispolaris@gmail.com>
2019-06-15 18:56:58 -0500

    Added game description to README, pseudo code to JS file.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,24 @@
-// Here's how the app works:
+// ======= PSEUDO CODE =======
+// DOCUMENT READY
+    // 1. Wins, losses, and totalScore is set to 0; 
 
-// There will be four crystals displayed as buttons on the page.
+// NEW GAME - Function
+    // 2. NumBer to guess is generated (chosenNumber) at the start of the game (Number between 19 and 120);
 
-// The player will be shown a random number at the start of the game.
+    // 3. Numbers for each crystal (crystal1, crystal2, crystal3, crystal14) are also generated at random (Number between 1 and 12);
+        // Each Crystal gets assigned a number
 
-// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+ // CLICK EVENT 
+    // 4. When user clicks on one crystal
+        // Add crystal number value to totalScore:
+        // Check for chosenNumber is  GREATER THAN OR EQUAL to total score
+            // If equal: Wins++
+                // RESET GAME - Function
+            // If  Greater than: Losses++
+                // RESET GAME - Function
+// RESET GAME - Function
+    // totalScore is set to 0;
+    // chosenNumber is generated;
+    // random numbers are generated for crystals
 
-// Your game will hide this amount until the player clicks a crystal.
-// When they do click one, update the player's score counter.
-// The player wins if their total score matches the random number from the beginning of the game.
-
-// The player loses if their score goes above the random number.
-
-// The game restarts whenever the player wins or loses.
-
-// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-
+// ----------
