06bd8d6e1968a5d84b2a0f32abf7c849aaa5f050
Santiago Enciso <whereispolaris@gmail.com>
2019-06-15 19:07:06 -0500

    Created global game variables and function that starts the game.

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@
 // DOCUMENT READY
     // 1. Wins, losses, and totalScore is set to 0; 
 
-// NEW GAME - Function
+// RESET (START) GAME - Function
     // 2. NumBer to guess is generated (chosenNumber) at the start of the game (Number between 19 and 120);
 
     // 3. Numbers for each crystal (crystal1, crystal2, crystal3, crystal14) are also generated at random (Number between 1 and 12);
@@ -13,12 +13,27 @@
         // Add crystal number value to totalScore:
         // Check for chosenNumber is  GREATER THAN OR EQUAL to total score
             // If equal: Wins++
-                // RESET GAME - Function
+                // RESET (START) GAME - Function
             // If  Greater than: Losses++
-                // RESET GAME - Function
-// RESET GAME - Function
+                // RESET (START) GAME - Function
+// RESET (START) GAME - Function
     // totalScore is set to 0;
     // chosenNumber is generated;
     // random numbers are generated for crystals
 
 // ----------
+
+
+var wins;
+var losses;
+var totalScore;
+var chosenNumber;
+
+function startGame() {
+    console.log("game starts");
+}
+
+startGame();
+
+
+
